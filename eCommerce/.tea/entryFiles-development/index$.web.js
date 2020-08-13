1f740b28f8c2959e8f288325ba357ccefc99f655
kabelo-baloyi <kabelo.baloyi@accenture.com>
2020-08-13 20:02:16 +0200

    Product List page layout

--- eCommerce/.tea/entryFiles-development/index$.web.js
+++ eCommerce/.tea/entryFiles-development/index$.web.js
@@ -3,6 +3,7 @@ require('./config$');
 
 require('../../node_modules/mini-antui/es/search-bar/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
 require('../../components/nav/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
+require('../../pages/list/list?hash=562c37c8dfe27e5c87c02199f96961b916f3f966');
 require('../../pages/index/index?hash=562c37c8dfe27e5c87c02199f96961b916f3f966');
 require('../../pages/checkout/checkout?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/cart/cart?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
